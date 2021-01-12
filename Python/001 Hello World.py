d87dd5b20bfc19dba7d7037a59c5b338162f4c84
LalithShravan <59380370+LalithShravan@users.noreply.github.com>
2021-01-12 10:47:27 -0800

    Hello World Program improvment - Intro to python

--- Python/001 Hello World.py
+++ Python/001 Hello World.py
@@ -1,5 +1,31 @@
-import math
+print("Hello World")
 
-x = math.pi
+x = 8 + 2
+print(x)
 
-print(f" Values:  {x}")
+'''
++ -> Addition
+- -> Subtraction
+* -> Mulitplication
+/ -> Division
+** -> Expontation
+'''
+
+print((7 + 9) * 2)
+print(7 + 9 *2)
+
+y = "Hello World!"
+print(y)
+
+# You can add strings. You can also us '' to make a string.
+y = y + ' I am new here!'
+print(y)
+
+# This wouldn't work becuse of the quotes messing with the string
+# print("He said," My name is python"")
+
+print("He said,\"My name is Python.\"")
+
+print("Hello %s" %"World!")
+
+print("Hello World!" * 5)
